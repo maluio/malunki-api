dfcba33c7339672d450e15f73c7d92df67a70a25
Malu <malu@example.com>
2017-07-30 13:43:24 +0200

    Remove pre-autowiring

--- src/AppBundle/Action/CardCreate.php
+++ src/AppBundle/Action/CardCreate.php
@@ -2,20 +2,16 @@
 namespace AppBundle\Action;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-// @todo: use DI to load Kernel service
-class CardCreate implements ContainerAwareInterface
+class CardCreate
 {
     private $kernel;
 
-    private $container;
-
-    public function __construct()
+    public function __construct(KernelInterface $kernel)
     {
-        //$this->kernel = $kernel;
+        $this->kernel = $kernel;
     }
 
     /**
@@ -33,12 +29,10 @@ class CardCreate implements ContainerAwareInterface
 
         $data->setReviewDate(new \DateTime());
 
-        //   if ($this->kernel->getEnvironment() === 'test'){
-        //     $data->setReviewDate(new \DateTime('2000-07-29T11:43:16+00:00'));
-        // }
-
-        if ($this->container->get( 'kernel' )->getEnvironment() === 'test'){
-            $data->setReviewDate = $data->getReviewDate()->modify('2000-07-29T11:43:16+00:00');
+        if ($this->kernel->getEnvironment() === 'test'){
+            $data->setReviewDate(
+                $data->getReviewDate()->modify('2000-07-29T11:43:16+00:00')
+            );
         }
 
         $data->setRepetition(0);
@@ -47,11 +41,5 @@ class CardCreate implements ContainerAwareInterface
 
         return $data;
     }
-
-    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
 
 }
